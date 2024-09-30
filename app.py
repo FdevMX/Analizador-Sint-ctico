ac27e98dc76c1e10a83ef8f8ce27bc6f2a1697d8
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-09-29 19:14:36 -0600

    add backend for flask

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from lexico import get_all_tokens
+from sintactico import parse_for_loop
+
+app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    code = request.json['code']
+    
+    # Análisis léxico
+    tokens = get_all_tokens(code)
+    token_list = [{"token": t[0], "lexeme": str(t[1]), "line": t[2]} for t in tokens]
+    
+    # Análisis sintáctico
+    parse_for_loop(code)
+    from sintactico import errors as syntax_errors
+    
+    return jsonify({
+        "tokens": token_list,
+        "errors": syntax_errors
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
