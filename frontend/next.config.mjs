7e7baaa99b3addbc7148769d153a6d4f868a4039
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-09-29 19:23:09 -0600

    added flask execution path

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: '/analyze',
+          destination: 'http://localhost:5000/analyze', // Asume que Flask está corriendo en el puerto 5000
+        },
+      ]
+    },
+    // Asegúrate de que el output sea 'standalone' para Vercel
+    output: 'standalone',
+}
+  
+  export default nextConfig;
