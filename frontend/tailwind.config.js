dd011afbdb78d47081ea6e648eae47ef5d3a1181
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-09-29 19:22:31 -0600

    added universal colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-gray': '#09090B', // This is the very dark gray color for the main background
+        'dark-gray-800': '#1E1E1E', // This is a slightly lighter gray for elements like textareas and output boxes
+        'dark-line-table': '#24242b',
+      },
+    },
   },
   plugins: [],
 }
